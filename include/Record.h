b6f859880f2669bf476356c7468536d223ef885d
Gleb Gerasimov <glebol2017@gmail.com>
2026-04-21 11:15:06 +0300

    update Record.h header

--- include/Record.h
+++ include/Record.h
@@ -5,10 +5,29 @@
 #ifndef SECURE_TOURISM_DB_SYSTEM_RECORD_H
 #define SECURE_TOURISM_DB_SYSTEM_RECORD_H
 
+#include <QDate>
 
 
-class Record {
+struct Record {
+    int id;
 
+    QString fullName;
+    QString homeAddress;
+    QString phoneNumber;
+
+    QDate startDate;
+    QDate endDate;
+
+    QString type;
+    QString country;
+
+    QString hotelName;
+    QString hotelAddress;
+
+    bool hasFlight;
+
+    int participants;
+    double price;
 };
 
 
