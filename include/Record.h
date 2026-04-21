7330a5cdfd0d5472c44d91d65b0ce5d8956ef025
Gleb Gerasimov <glebol2017@gmail.com>
2026-04-21 04:09:07 +0300

    add record header and class

---
+++ include/Record.h
@@ -0,0 +1,16 @@
+//
+// Created by glebo on 21.04.2026.
+//
+
+#ifndef SECURE_TOURISM_DB_SYSTEM_RECORD_H
+#define SECURE_TOURISM_DB_SYSTEM_RECORD_H
+
+
+
+class Record {
+
+};
+
+
+
+#endif //SECURE_TOURISM_DB_SYSTEM_RECORD_H
